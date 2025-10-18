92122d37f04a7a932eba63676dc60e362efee3e5
HuyVeo <Huyveo37@gmail.com>
2025-10-18 15:35:55 +0700

    Them chroma

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+
+from run import chatModel
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
@@ -11,55 +13,43 @@ import os
 from langchain_ollama import ChatOllama
 
 app = Flask(__name__)
-
-
 load_dotenv()
 
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
 
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
-
 embeddings = download_hugging_face_embeddings()
-
 index_name = "medical-chatbot"
 # Embed each chunk and upsert the embeddings into your Pinecone index.
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
-
-
-
-
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-# chatModel = ChatOpenAI(model="gpt-4.1-nano")
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", system_prompt),
-#         ("human", "{input}"),
-#     ]
-# )
+chatModel = ChatOpenAI(model="gpt-4.1-nano" ,  temperature=0.3,
+            max_tokens=100,)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
 
-chatModel = ChatOllama(model="gemma2", base_url="http://localhost:11434")
-prompt = ChatPromptTemplate.from_messages([
-    ("system", system_prompt),
-    ("human", "{input}"),
-])
+
+
+# chatModel = ChatOllama(model="gemma2", base_url="http://localhost:11434")
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", system_prompt),
+#     ("human", "{input}"),
+# ])
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
-
 @app.route("/")
 def index():
     return render_template('chat.html')
-
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
